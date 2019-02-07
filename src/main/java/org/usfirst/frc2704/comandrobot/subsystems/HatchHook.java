90c72de31eb7017e0f0705f728b600471638c256
Undoified <25871683+undoified@users.noreply.github.com>
2019-02-06 20:09:10 -0600

    Preliminary hook code

---
+++ src/main/java/org/usfirst/frc2704/comandrobot/subsystems/HatchHook.java
@@ -0,0 +1,60 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc2704.comandrobot.subsystems;
+import org.usfirst.frc2704.comandrobot.commands.*;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Add your docs here.
+ */
+public class HatchHook extends Subsystem {
+
+  private DoubleSolenoid solenoid1;
+  private DoubleSolenoid solenoid2;
+  private Compressor compressor;
+
+  public HatchHook() {
+    compressor = new Compressor(0);
+    addChild("compressor", compressor);
+    solenoid1 = new DoubleSolenoid(1, 2);
+    addChild("solenoid", solenoid1);
+    solenoid2 = new DoubleSolenoid(3, 4);
+    addChild("solenoid2", solenoid2);
+  }
+
+  public void turnOn() {
+    compressor.setClosedLoopControl(true);
+  }
+
+  public void turnOff() {
+    compressor.setClosedLoopControl(false);
+  }
+
+  public void extend() {
+    solenoid1.set(DoubleSolenoid.Value.kForward);
+    solenoid2.set(DoubleSolenoid.Value.kForward);
+    solenoid1.set(DoubleSolenoid.Value.kOff);
+    solenoid2.set(DoubleSolenoid.Value.kOff);
+  }
+
+  public void retract() {
+    solenoid1.set(DoubleSolenoid.Value.kReverse);
+    solenoid2.set(DoubleSolenoid.Value.kReverse);
+    solenoid1.set(DoubleSolenoid.Value.kOff);
+    solenoid2.set(DoubleSolenoid.Value.kOff);
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
