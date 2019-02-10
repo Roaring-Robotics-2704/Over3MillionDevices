abd5f0d1ff6db11c4f570ec6bd14ddd84af610b3
Undoified <25871683+undoified@users.noreply.github.com>
2019-02-10 12:18:32 -0600

    Put ControlHookWithButtons.java back because someone deleted it. >:(

---
+++ src/main/java/org/usfirst/frc2704/comandrobot/commands/ControlHookWithButtons.java
@@ -0,0 +1,52 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc2704.comandrobot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc2704.comandrobot.Robot;
+
+public class ControlHookWithButtons extends Command {
+  public ControlHookWithButtons() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    Robot.hatchhook.turnOn();
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    if (Robot.oi.joystick1_button1.get() == true) {
+      Robot.hatchhook.extend();
+    }
+    if (Robot.oi.joystick1_button2.get() == true) {
+      Robot.hatchhook.retract();
+    }
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+    Robot.hatchhook.turnOff();
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
